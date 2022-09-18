885199b5bcc9a753698859c3ef9f9238d2f7b306
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-18 10:46:03 -0500

    added code and comments

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ generateBtn.addEventListener("click", function (event) {
 function promptUser(event) {
   // added a while to set a loop if passLen isnt't met
   var passLen = 0;
-  while (passLen < 8 || passLen > 128) {
+  while (passLen < 8 || passLen > 128 || isNaN(passPrompt)) {
     var passPrompt = window.prompt("How long should the password be ?");
     passLen = parseInt(passPrompt);
    //isNaN for if the length of password is canceled
